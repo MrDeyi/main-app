d21e4bcbcebd95ee38df96854abc0b85752c15c4
mandilake <mandilake@anylytical.co.za>
2025-01-29 14:02:13 +0200

    test pipeline

---
+++ Main_app/src/test/java/com/main_app/MainAppApplicationTests.java
@@ -0,0 +1,15 @@
+package com.main_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainAppApplicationTests
+{
+
+  @Test
+  void contextLoads()
+  {
+  }
+
+}
